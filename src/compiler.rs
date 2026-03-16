76ff486c8dea109c6f8857464d22c18799447259
Jackson Brough <jacksontbrough@gmail.com>
2026-03-15 23:41:35 -0600

    Make dangling transclusions and links error a bit better

--- src/compiler.rs
+++ src/compiler.rs
@@ -162,7 +162,7 @@ impl Compiler {
                 .expect("bug: node in transclusion graph has no file entry");
             let name = self.interner.name(destination);
             let message = if self.removed.contains(&destination) {
-                eco_format!("dangling transclusion: {name} was deleted this cycle")
+                eco_format!("dangling transclusion: {name} was deleted")
             } else {
                 eco_format!("dangling transclusion: {name} is not defined")
             };
@@ -185,14 +185,16 @@ impl Compiler {
                 .copied()
                 .expect("bug: node in link graph has no file entry");
             let name = self.interner.name(destination);
+            let message = if self.removed.contains(&destination) {
+                eco_format!("dangling link: {name} was deleted")
+            } else {
+                eco_format!("dangling link: {name} is not defined")
+            };
 
             self.process_diagnostics
                 .entry(file_id)
                 .or_default()
-                .push(SourceDiagnostic::warning(
-                    Span::detached(),
-                    eco_format!("dangling link: {name}"),
-                ));
+                .push(SourceDiagnostic::warning(Span::detached(), message));
         }
 
         // Compute the render set
