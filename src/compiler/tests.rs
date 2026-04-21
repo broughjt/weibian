9b898d38a6064854bd06e501c7ed935f0496e0da
Jackson Brough <jacksontbrough@gmail.com>
2026-04-20 21:45:37 -0600

    Stub incremental matches stateless

--- src/compiler/tests.rs
+++ src/compiler/tests.rs
@@ -1,2 +1,30 @@
+use proptest_state_machine::StateMachineTest;
+
 mod mock;
 mod stateless;
+
+struct IncrementalMatchesStateless;
+
+impl StateMachineTest for IncrementalMatchesStateless {
+    type SystemUnderTest;
+    type Reference;
+
+    fn init_test(
+        ref_state: &<Self::Reference as proptest_state_machine::ReferenceStateMachine>::State,
+    ) -> Self::SystemUnderTest {
+        todo!()
+    }
+
+    fn apply(
+        state: Self::SystemUnderTest,
+        ref_state: &<Self::Reference as proptest_state_machine::ReferenceStateMachine>::State,
+        transition: <Self::Reference as proptest_state_machine::ReferenceStateMachine>::Transition,
+    ) -> Self::SystemUnderTest {
+        todo!()
+    }
+}
+
+prop_state_machine! {
+    #[test]
+    fn incremental_matches_stateless(sequential 1..20 => IncrementalMatchesStateless);
+}
