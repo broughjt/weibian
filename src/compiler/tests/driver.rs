40231cf6dd46f144b9b0ddb1004b52b01cf7564e
Jackson Brough <jacksontbrough@gmail.com>
2026-05-04 16:45:53 -0600

    Batched?

--- src/compiler/tests/driver.rs
+++ src/compiler/tests/driver.rs
@@ -5,30 +5,6 @@ use std::sync::{
 
 use proptest_state_machine::{ReferenceStateMachine, StateMachineTest};
 
-// /// A cheap rip-off of [`StateMachineTest`] that is new and improved.
-// pub trait StateMachineTestDeluxe {
-//     type Implementation;
-//     type Specification: ReferenceStateMachine;
-
-//     fn initial_state(
-//         specification_state: &<Self::Specification as ReferenceStateMachine>::State,
-//     ) -> Self::Implementation;
-
-//     fn apply(
-//         implementation_state: Self::Implementation,
-//         specification_state_before: &<Self::Specification as ReferenceStateMachine>::State,
-//         specification_state_after: &<Self::Specification as ReferenceStateMachine>::State,
-//         transition: <Self::Specification as ReferenceStateMachine>::Transition,
-//     ) -> Self::Implementation;
-
-//     fn check_invariants(
-//         implementation_state: &Self::Implementation,
-//         specification: &<Self::Specification as ReferenceStateMachine>::State,
-//     ) {
-//         let _ = (implementation_state, specification);
-//     }
-// }
-
 pub fn run_sequential<T: StateMachineTest>(
     mut specification_state: <T::Reference as ReferenceStateMachine>::State,
     transitions: Vec<<T::Reference as ReferenceStateMachine>::Transition>,
@@ -50,44 +26,48 @@ pub fn run_sequential<T: StateMachineTest>(
         T::check_invariants(&implementation_state, &specification_state);
     }
 
-    // T::teardown(state);
+    T::teardown(implementation_state, specification_state);
 }
 
-// pub fn run_batched<T: StateMachineTestDeluxe>(
-//     mut specification_state: <T::Specification as ReferenceStateMachine>::State,
-//     transitions: Vec<<T::Specification as ReferenceStateMachine>::Transition>,
-//     mut seen_counter: Option<Arc<AtomicUsize>>,
-//     batch_sizes: Vec<usize>,
-// ) {
-//     let batches = {
-//         let mut transitions = transitions.into_iter();
-//         batch_sizes
-//             .into_iter()
-//             .map(|size| transitions.by_ref().take(size).collect::<Vec<_>>())
-//             .take_while(|batch| !batch.is_empty())
-//             .collect::<Vec<Vec<<T::Specification as ReferenceStateMachine>::Transition>>>()
-//     };
+pub trait StateMachineTestBatched: StateMachineTest {
+    fn apply_batch(
+        implementation_state: Self::SystemUnderTest,
+        specification_state: &<Self::Reference as ReferenceStateMachine>::State,
+    ) -> Self::SystemUnderTest;
+}
 
-//     let mut implementation_state = T::initial_state(&specification_state);
+pub fn run_batched<T: StateMachineTestBatched>(
+    mut specification_state: <T::Reference as ReferenceStateMachine>::State,
+    transitions: Vec<<T::Reference as ReferenceStateMachine>::Transition>,
+    mut seen_counter: Option<Arc<AtomicUsize>>,
+    batch_sizes: Vec<usize>,
+) {
+    let batches = {
+        let mut transitions = transitions.into_iter();
+        batch_sizes
+            .into_iter()
+            .map(|size| transitions.by_ref().take(size).collect::<Vec<_>>())
+            .take_while(|batch| !batch.is_empty())
+            .collect::<Vec<Vec<<T::Reference as ReferenceStateMachine>::Transition>>>()
+    };
 
-//     T::check_invariants(&implementation_state, &specification_state);
+    let mut implementation_state = T::init_test(&specification_state);
 
-//     for batch in batches {
-//         for transition in batch {
-//             if let Some(counter) = seen_counter.as_mut() {
-//                 counter.fetch_add(1, atomic::Ordering::SeqCst);
-//             }
-//             let specification_state_next = <T::Specification as ReferenceStateMachine>::apply(
-//                 specification_state.clone(),
-//                 &transition,
-//             );
-//             implementation_state = T::apply(
-//                 implementation_state,
-//                 &specification_state_next,
-//                 &specification_state_next,
-//                 transition,
-//             );
-//             specification_state = specification_state_next;
-//         }
-//     }
-// }
+    T::check_invariants(&implementation_state, &specification_state);
+
+    for batch in batches {
+        for transition in batch {
+            if let Some(counter) = seen_counter.as_mut() {
+                counter.fetch_add(1, atomic::Ordering::SeqCst);
+            }
+            specification_state =
+                <T::Reference as ReferenceStateMachine>::apply(specification_state, &transition);
+            implementation_state = T::apply(implementation_state, &specification_state, transition);
+        }
+        implementation_state = T::apply_batch(implementation_state, &specification_state);
+
+        T::check_invariants(&implementation_state, &specification_state);
+    }
+
+    T::teardown(implementation_state, specification_state);
+}
